95f09822e32d2ca4d5ee097fc402ef32f4c9e27e
Mop Index <tugjvntugjvn@gmail.com>
2019-05-25 19:08:52 -0700

    scheduler complete

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const schedule = require('node-schedule');
+const rule = new schedule.RecurrenceRule();
+rule.hour = 0;
+
+let probability = Math.floor(Math.random() * 99);
+schedule.scheduleJob(rule, () => {
+    probability = Math.floor(Math.random() * 99);
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
+})
+app.get('/probability', (req, res) => {
+    res.json(probability);
 })
 app.listen(8080, () => {
     console.log('Server listening on port 8080')
